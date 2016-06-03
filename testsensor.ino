7268326585735c1a0f8abc5a864e6169ea257636
JohannesKauffmann <johanneskauffmann@hotmail.com>
2016-06-03 13:00:59 +0000

    Add files via upload

---
+++ testsensor.ino
@@ -0,0 +1,24 @@
+int echoPin = 13; //echo
+int trigPin = 12; //trigger
+int pin1 = 11;
+
+void setup()
+{
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(pin1, OUTPUT);
+}
+
+void loop()
+{
+  //het volgende zend een pulse naar de trigpin en zet deze daarna weer low
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, LOW);
+
+  //definieer een variabele genaamd duration: dit is hoelang de echo erover doet om een signaal terug te sturen (om op HIGH te gaan staan)
+  duration = pulseIn(echoPin, HIGH);
+
+}
